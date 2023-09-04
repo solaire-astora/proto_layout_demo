1b4797174fbe2284efe64f8fe6bd514d97c83318
solaire-astora <solaire.astora47@gmail.com>
2023-09-04 14:16:39 +0800

    test

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"bytes"
+	"demo/testpb"
 	"fmt"
 	"os"
 
 	"google.golang.org/protobuf/encoding/protowire"
+	"google.golang.org/protobuf/proto"
 )
+
+func marshal() {
+	msg := testpb.MyMessage{
+		Name: "123",
+		Age:  10,
+	}
+	bytes1, _ := proto.Marshal(&msg)
+
+	bytes2 := []byte{}
+	bytes2 = protowire.AppendTag(bytes2, 1, protowire.BytesType)
+	bytes2 = protowire.AppendString(bytes2, "123")
+	bytes2 = protowire.AppendTag(bytes2, 2, protowire.VarintType)
+	bytes2 = protowire.AppendVarint(bytes2, 10)
+
+	fmt.Println(bytes.Equal(bytes1, bytes2))
+}
 
 func main() {
 	bytes, err := os.ReadFile("test.bin")
@@ -23,4 +42,6 @@ func main() {
 		}
 		idx += ret
 	}
+
+	marshal()
 }
